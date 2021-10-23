2c34cc36d50f8907b06d7c384cebd950eec02a8b
hpteach2021 <hooper.rob@gmail.com>
2021-10-23 20:18:14 +0100

    changed links on Nav

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -18,9 +18,9 @@ const Nav = () => {
       {showNav && <MobileNav handleClick={toggleNav} />}
       <div className="topnav">
         <Link to ="/"><p  className="topnav__item" href="#home">Home</p></Link>
-        <Link to ="/my-work"><p className="topnav__item" href="#news">Work</p></Link>
-        <Link to ="/my-skills"><p className="topnav__item" href="#contact">Skills</p></Link>
-        <Link to ="/contact-me"><p className="topnav__item topnav__contact" href="#about">Contact</p></Link>
+        <Link to ="/rob-hooper-portfolio/my-work"><p className="topnav__item" href="#news">Work</p></Link>
+        <Link to ="/rob-hooper-portfolio/my-skills"><p className="topnav__item" href="#contact">Skills</p></Link>
+        <Link to ="/rob-hooper-portfolio/contact-me"><p className="topnav__item topnav__contact" href="#about">Contact</p></Link>
   
       </div>
 
